8fffb590e4fbde4e8c96f823eeb8c4d18ca3a641
Harsh Patel <harshaudi27@gmail.com>
2019-07-31 04:18:23 +0530

    Restructured navigation bar

--- app/src/main/java/com/harsh/kitchensinkapp/MainActivity.java
+++ app/src/main/java/com/harsh/kitchensinkapp/MainActivity.java
@@ -244,9 +244,9 @@ public class MainActivity extends AppCompatActivity
                 }
             });
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_debounce) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_fuzziness) {
 
         }
 
