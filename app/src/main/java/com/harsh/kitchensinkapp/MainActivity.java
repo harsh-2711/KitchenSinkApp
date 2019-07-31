ad1bc1d13b1621098e012900b45f4bb63f615680
Harsh Patel <harshaudi27@gmail.com>
2019-07-31 05:43:50 +0530

    Added default suggestions option

--- app/src/main/java/com/harsh/kitchensinkapp/MainActivity.java
+++ app/src/main/java/com/harsh/kitchensinkapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.harsh.searchwidget.Builder.DefaultClientSuggestions;
 import com.harsh.searchwidget.Fragments.VoicePermissionDialogFragment;
 import com.harsh.searchwidget.Model.ClientSuggestionsModel;
 import com.harsh.searchwidget.Model.SearchPropModel;
@@ -680,6 +681,58 @@ public class MainActivity extends AppCompatActivity
                     .setHighlight(true)
                     .setCategoryField("tags")
                     .setTopEntries(2)
+                    .build();
+
+            searchBar.setSearchIconVisibility(true);
+            searchBar.setClearIconVisibility(true);
+            searchBar.setSearchIcon(R.drawable.ic_magnify_black_48dp);
+            searchBar.setClearIcon(R.drawable.ic_close_black_48dp);
+            searchBar.setSpeechMode(false);
+            searchBar.setRoundedSearchBarEnabled(false);
+            searchBar.setMaxSuggestionCount(5);
+
+            searchBar.startSearch(searchPropModel, new SearchBar.ItemClickListener() {
+                @Override
+                public void onClick(View view, int position, ClientSuggestionsModel result) {
+
+                }
+
+                @Override
+                public void onLongClick(View view, int position, ClientSuggestionsModel result) {
+
+                }
+            });
+
+        } else if (id == R.id.nav_max_default_suggestions) {
+
+            searchBar.disableSearch();
+
+            // Making list of default suggestions
+            ArrayList<String> suggestions = new ArrayList<>();
+            suggestions.add("Puma T-Shirt");
+            suggestions.add("Apple iPhone XS");
+            suggestions.add("Nike Trousers");
+
+            // Making list of default categories to be displayed
+            ArrayList<String> categories = new ArrayList<>();
+            categories.add("T-Shirt");
+            categories.add("Mobiles");
+
+            // Setting default suggestions
+            ArrayList<ClientSuggestionsModel> defaultSuggestions = new DefaultClientSuggestions(suggestions).setCategories(categories).build();
+
+            final SearchPropModel searchPropModel = searchBar.setSearchProp("Demo Widget", dataFields)
+                    .setQueryFormat("or")
+                    .setHighlight(true)
+                    .setRedirectIcon(true)
+                    .setSearchResultImage(true)
+                    .setHitsEnabled(false)
+                    .setDebounce(300)
+                    .setFuzziness("AUTO")
+                    .setHighlight(true)
+                    .setCategoryField("tags")
+                    .setTopEntries(2)
+                    .setDefaultSuggestions(defaultSuggestions)
                     .build();
 
             searchBar.setSearchIconVisibility(true);
